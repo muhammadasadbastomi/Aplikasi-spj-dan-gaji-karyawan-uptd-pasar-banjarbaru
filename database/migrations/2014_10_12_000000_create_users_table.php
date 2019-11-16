d344730de1d3e5a6e7b12514d576e2cd24b7b0da
muhammadasadbastomi <tomycules@gmail.com>
2019-11-16 12:08:38 +0800

    recreate users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,10 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('role')->default(1);
+            $table->tinyInteger('status')->default(0);
+            $table->string('foto')->length('255')->default('default.jpg');
             $table->rememberToken();
             $table->timestamps();
         });
