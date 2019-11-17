32fe111b10adeffea5a706072f199c72dd39855e
muhammadasadbastomi <tomycules@gmail.com>
2019-11-17 18:45:42 +0800

    create APIController

---
+++ app/Http/Controllers/Auth/API/APIController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+
+class APIController extends Controller
+{
+    protected function returnController($status, $data){
+        if (!is_array($data) && json_decode($data) != null) {
+            $data = json_decode($data);
+        }
+
+        return response()->json([
+            'status' => $status,
+            'data' => $data,
+        ]);
+    }
+}
