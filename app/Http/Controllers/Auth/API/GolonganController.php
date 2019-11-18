556996102246006de2e5eb6c9882e947c144dfd7
muhammadasadbastomi <tomycules@gmail.com>
2019-11-18 20:35:33 +0800

    add method crud golongan

--- app/Http/Controllers/Auth/API/GolonganController.php
+++ app/Http/Controllers/Auth/API/GolonganController.php
@@ -17,8 +17,83 @@ class GolonganController extends APIController
             if (!$golongan) {
                 return $this->returnController("error", "failed get golongan data");
             }
-            Redis::set("golongan:all", $golongan);
+            Redis::set("golongan:all", $golongan);            
+
         }
         return $this->returnController("ok", $golongan);
+    }
+
+    public function find($uuid){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
+        }
+
+        $golongan = Redis::get("golongan:$id");
+        if (!$golongan) {
+            $golongan = golongan::find($id);
+            if (!$golongan){
+                return $this->returnController("error", "failed find data golongan");
+            }
+            Redis::set("golongan:$id", $golongan);
+        }
+
+        return $this->returnController("ok", $golongan);
+    }
+
+    public function create(Request $req){
+        $create = golongan::create($req->all());
+        if (!$create) {
+            return $this->returnController("error", "failed create data golongan");
+        }
+        Redis::del("golongan:all");
+        return $this->returnController("ok", $create);
+    }
+
+    public function update($uuid, Request $req){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
+        }
+
+        $golongan = golongan::find($id);
+        if (!$golongan) {
+            return $this->returnController("error", "failed find data golongan");
+        }
+
+        $update = $golongan->update($req->all());
+        if (!$update) {
+            return $this->returnController("error", "failed find data golongan");
+        }
+
+        Redis::del("golongan:all");
+        Redis::set("golongan:$id", $update);
+
+        return $this->returnController("ok", $update);
+    }
+
+    public function delete($uuid){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
+        }
+
+        $golongan = golongan::find($id);
+        if (!$golongan) {
+            return $this->returnController("error", "failed find data golongan");
+        }
+
+        // Need to check realational
+        // If there relation to other data, return error with message, this data has relation to other table(s)
+
+        $delete = $golongan->delete();
+        if (!$delete) {
+            return $this->returnController("error", "failed delete data golongan");
+        }
+
+        Redis::del("golongan:all");
+        Redis::del("golongan:$id");
+
+        return $this->returnController("ok", "success delete data golongan");
     }
 }
