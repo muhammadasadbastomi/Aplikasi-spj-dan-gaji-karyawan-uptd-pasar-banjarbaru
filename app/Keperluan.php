05bd2398efb89f12c5057768dcfaea495712d40a
muhammadasadbastomi <tomycules@gmail.com>
2019-11-29 09:49:40 +0700

    add relation many to 1 pajak

--- app/Keperluan.php
+++ app/Keperluan.php
@@ -8,4 +8,8 @@ class Keperluan extends Model
 {
     protected $fillable = ['uuid','keperluan'];
     protected $hidden = ['id','pajak_id'];
+
+    public function pajak(){
+        return $this->HasOne('App\Pajak');
+      }
 }
