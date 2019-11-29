0e4d2dfc6d90c647d4b79ba5222616c1820b8ec7
muhammadasadbastomi <tomycules@gmail.com>
2019-11-29 09:53:12 +0700

    initialize required library

--- app/Http/Controllers/API/KeperluanController.php
+++ app/Http/Controllers/API/KeperluanController.php
@@ -1,8 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Keperluan;
+use HCrypt;
+use Illuminate\Support\Facades\Redis;
 
 class KeperluanController extends Controller
 {
