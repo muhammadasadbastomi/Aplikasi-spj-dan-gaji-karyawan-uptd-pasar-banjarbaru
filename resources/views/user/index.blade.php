4730ba4f093c4b2f9babcd7e1c9315cac02b365b
tri angga utama <trianggategarutama@gmail.com>
2020-01-07 14:29:35 +0800

    tambah script crud user

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -22,31 +22,22 @@
                                 <h5 class="card-title">Tabel Data User</h5>
                                 <div class="text-right">
                                 <a href="" class="btn btn-outline-info"><i class="mdi mdi-printer"></i> cetak</a>
-                                <a href="" class="btn btn-outline-danger" data-toggle="modal" data-target="#tambahData"><i class="mdi mdi-add"></i>+ tambah data</a>               
+                                <a href="" class="btn btn-outline-danger" data-toggle="modal" data-target="#tambahData" id="tambah"><i class="mdi mdi-add"></i>+ tambah data</a>               
                                 </div>
                                 <br>
                                 <div class="table-responsive">
-                                    <table id="zero_config" class="table table-striped table-bordered">
+                                    <table id="datatable" class="table table-striped table-bordered">
                                         <thead>
                                             <tr>
-                                                <th>No</th>
                                                 <th>Username</th>
                                                 <th class="text-center">Aksi</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>admin123</td>
-                                                <td class="text-center">
-                                                    <a href="{{Route('standardHargaEdit')}}" class="btn btn-info"><i class="mdi mdi-pencil"></i> </a>
-                                                    <a href="" class="btn btn-danger"><i class="mdi mdi-popcorn"></i> </a>
-                                                </td>
-                                            </tr>
+                            
                                         </tbody>
                                         <tfoot>
                                             <tr>
-                                                <th>No</th>
                                                 <th>Username</th>
                                                 <th class="text-center">Aksi</th>
                                             </tr>
@@ -65,7 +56,7 @@
         </div>
 
 <!-- Modal -->
-<div class="modal fade" id="tambahData" tabindex="-1" role="dialog" aria-labelledby="tambahData" aria-hidden="true">
+<div class="modal fade" id="mediumModal" tabindex="-1" role="dialog" aria-labelledby="tambahData" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -75,29 +66,169 @@
         </button>
       </div>
       <div class="modal-body">
-      <div class="form-group m-t-20">
-        <label> Nama</label>
-        <input type="text" class="form-control date-inputmask" id="date-mask" placeholder="">
-     </div>
-     <div class="form-group">
-        <label>Harga Satuan </label>
-        <input type="text" class="form-control phone-inputmask" id="phone-mask" placeholder="">
-     </div>
-     <div class="form-group">
-        <label>Keperluan </label>
-        <select name="" id="" class="form-control">
-            <option value="">Belanja Alat Tulis Kantor</option>
-            <option value="">Makan Minum Oprasional</option>
-            <option value="">Belanja Peralatan dan Perlengkapan Komputer</option>
-            <option value="">Gajih Honorer</option>
-        </select>
-     </div>
-    </div>
+      <form action="" method="post">
+        <div class="form-group m-t-20">
+            <label>Username</label>
+            <input type="text" class="form-control date-inputmask" id="username" name="username" placeholder="">
+        </div>
+        <div class="form-group">
+            <label>Password (isi jika ingin merubah password)</label>
+            <input type="password" class="form-control phone-inputmask" id="password" name="password" placeholder="">
+        </div>
+        </div>
         <div class="modal-footer">
             <a href=""  class="" data-dismiss="modal"><i class="mdi mdi-close-circle-outline"></i> Batal</a>
-            <button type="button" class="btn btn-primary"> <i class="mdi mdi-content-save-outline"></i> Simpan</button>
+            <button type="submit" name="submit" id="btn-form" class="btn btn-primary"> <i class="mdi mdi-content-save-outline"></i> Simpan</button>
         </div>
+        </form>
     </div>
   </div>
 </div>
+@endsection
+
+@section('script') 
+    <script>
+        //fungsi hapus
+        hapus = (uuid, name) =>{
+            let csrf_token=$('meta[name="csrf_token"]').attr('content');
+            Swal.fire({
+                        title: 'apa anda yakin?',
+                        text: " Menghapus data  user" + name,
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'hapus data',
+                        cancelButtonText: 'batal',
+                        reverseButtons: true
+                    }).then((result) => {
+                        if (result.value) {
+                            $.ajax({
+                                url : "{{ url('/api/user')}}" + '/' + uuid,
+                                type : "POST",
+                                data : {'_method' : 'DELETE', '_token' :csrf_token},
+                                success: function (response) {
+                                    Swal.fire({
+                                    position: 'top-end',
+                                    icon: 'success',
+                                    title: 'Data Berhasil Dihapus',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                })
+                            $('#datatable').DataTable().ajax.reload(null, false);
+                        },
+                    })
+                    } else if (result.dismiss === swal.DismissReason.cancel) {
+                        Swal.fire(
+                        'Dibatalkan',
+                        'data batal dihapus',
+                        'error'
+                        )
+                    }
+                })
+            }
+    
+            //event btn tambah klik
+            $('#tambah').click(function(){
+                $('.modal-title').text('Tambah Data');
+                $('#username').val('');
+                $('#password').val('');  
+                $('#btn-form').text('Simpan Data');
+                $('#mediumModal').modal('show');
+            })
+
+            //event btn edit klik        
+            edit = uuid =>{
+                $.ajax({
+                    type: "GET",
+                    url: "{{ url('/api/user')}}" + '/' + uuid,
+                    beforeSend: false,
+                    success : function(returnData) {
+                        $('.modal-title').text('Edit Data');
+                        $('#id').val(returnData.data.uuid);
+                        $('#username').val(returnData.data.username); 
+                        $('#btn-form').text('Ubah Data');
+                        $('#mediumModal').modal('show');
+                    }
+                })
+            }
+
+            //fungsi render datatable
+            $(document).ready(function() {
+                $('#datatable').DataTable( {
+                    responsive: true,
+                    processing: true,
+                    serverSide: false,
+                    searching: true,
+                    ajax: {
+                        "type": "GET",
+                        "url": "{{route('API.user.get')}}",
+                        "dataSrc": "data",
+                        "contentType": "application/json; charset=utf-8",
+                        "dataType": "json",
+                        "processData": true
+                    },
+                    columns: [
+                        {"data": "username"},
+                        {data: null , render : function ( data, type, row, meta ) {
+                            let uuid = row.uuid;
+                            let name = row.name;
+                            return type === 'display'  ?
+                            '<button onClick="edit(\''+uuid+'\')" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#editmodal"><i class="mdi mdi-pencil"></i></button> <button onClick="hapus(\'' + uuid + '\',\'' + name + '\')" class="btn btn-sm btn-danger" > <i class="mdi mdi-popcorn"></i></button>':
+                        data;
+                        }}
+                    ]
+                });
+
+                //event form submit            
+                $("form").submit(function (e) {
+                    e.preventDefault()
+                    let form = $('#modal-body form');
+                    if($('.modal-title').text() == 'Edit Data'){
+                        let url = '{{route("API.user.update", '')}}'
+                        let id = $('#id').val();
+                        $.ajax({
+                            url: url+'/'+id,
+                            type: "put",
+                            data: $(this).serialize(),
+                            success: function (response) {
+                                form.trigger('reset');
+                                $('#mediumModal').modal('hide');
+                                $('#datatable').DataTable().ajax.reload();
+                                Swal.fire({
+                                    position: 'top-end',
+                                    icon: 'success',
+                                    title: 'Data Berhasil Tersimpan',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                })
+                            },
+                            error:function(response){
+                                console.log(response);
+                            }
+                        })
+                    }else{
+                        $.ajax({
+                            url: "{{Route('API.user.create')}}",
+                            type: "post",
+                            data: $(this).serialize(),
+                            success: function (response) {
+                                form.trigger('reset');
+                                $('#mediumModal').modal('hide');
+                                $('#datatable').DataTable().ajax.reload();
+                                Swal.fire({
+                                    position: 'top-end',
+                                    icon: 'success',
+                                    title: 'Your work has been saved',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                })
+                            },
+                            error:function(response){
+                                console.log(response);
+                            }
+                        })
+                    }
+                } );
+                } );
+    </script>
 @endsection
